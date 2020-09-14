962f68958e58628d797fbbb7217049bba9c4f355
burcj3622 <burcj3622@wrdsb.ca>
2020-09-14 14:18:38 +0000

    tempature converter

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
     
+    //create a Scanner for user input
+   Scanner input = new Scanner(System.in);
+    
+
+    // ask user for temp
+    System.out.println("Please enter a temperature you would like to convert in degrees Celsius.");
+
+    // variable Celsius is the temp
+    int C = input.nextInt();
+
+  // Declare and calculate the temp
+  int F=(C*9)/5 +32;
+
+  //displayes the conversion
+System.out.println(C + " is the same as " + F);
   }
 }
